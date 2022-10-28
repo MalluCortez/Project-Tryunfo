258900fd22d63421fc463dc24a1da1e0a2183b0a
MalluCortez <maria.santana@outlook.com.br>
2022-10-28 16:42:57 -0300

    requisito 1 feito

---
+++ src/components/Form.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" data-testid="name-input"/>
+        <textarea data-testid="description-input" cols="30" rows="10"></textarea>
+        <input type="number" data-testid="attr1-input"/>
+        <input type="number" data-testid="attr2-input"/>
+        <input type="number" data-testid="attr3-input"/>
+        <input type="text" data-testid="image-input"/>
+        <select data-testid="rare-input">
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+        </select>
+        <input type="checkbox" data-testid="trunfo-input"/>
+        <button data-testid="save-button">Salvar</button>
+
+      </div>
+    )
+  }
+}
+
+export default Form
