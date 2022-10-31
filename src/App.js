554ca3f5c67b27155d51beac2a7cb36174d2106e
MalluCortez <maria.santana@outlook.com.br>
2022-10-31 15:49:18 -0300

    requisito 5 parcialmente feito

--- src/App.js
+++ src/App.js
@@ -13,12 +13,51 @@ class App extends React.Component {
     cardRare: '',
     cardTrunfo: '',
     hasTrunfo: false,
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
+  };
+
+  verificationText = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    const isValidText = cardName.length === 0 || cardDescription.length === 0
+    || cardImage.length === 0 || cardRare.length === 0;
+    return isValidText;
+  };
+
+  verificationNumber = () => {
+    let { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    cardAttr1 = Number(cardAttr1);
+    cardAttr2 = Number(cardAttr2);
+    cardAttr3 = Number(cardAttr3);
+    console.log(typeof cardAttr1);
+    const cardAttrValue = 90;
+    const sumAttr = 210;
+    const isValidNumber = cardAttr1 >= cardAttrValue || cardAttr2 >= cardAttrValue
+    || cardAttr3 >= cardAttrValue
+    || (cardAttr1 + cardAttr2 + cardAttr3) >= sumAttr;
+    const noNegativeNumbers = cardAttr1 < 0
+    || cardAttr2 < 0 || cardAttr3 < 0;
+
+    return isValidNumber || noNegativeNumbers;
+  };
+
+  allVerication = () => {
+    if (this.verificationText() || this.verificationNumber()) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+      return;
+    }
+    this.setState({
+      isSaveButtonDisabled: false,
+    });
   };
 
   onInputChange = (event) => {
+    const { name, value } = event.target;
     this.setState({
-      [event.target.name]: event.target.value,
+      [name]: value,
+    }, () => {
+      this.allVerication();
     });
   };
 
